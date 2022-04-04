3491be61260b40e0cca889f674c1125d55500dc8
Mattias Ekdahl <matekd@student.chalmers.se>
2022-04-04 21:00:10 +0200

    Clean up variable naming

--- RepoMiner.bat
+++ RepoMiner.bat
@@ -1,21 +1,22 @@
 @echo off
 set /p url="Enter your git repository link here: "
-set name_part=%url%
+
 set origin=%CD%
 
-if "%url:~0,5%" == "https" (set name_part=%url:~19%)
-for /f "tokens=2 delims=/" %%a in ("%name_part%") do (
-  set AFTER_SLASH=%%a
+if "%url:~0,5%" == "https" (set user_repo=%url:~19%) else (set user_repo=%url:~15%)
+
+for /f "tokens=2 delims=/" %%a in ("%user_repo%") do (
+  set temp=%%a
 )
 
-for /f "tokens=1 delims=." %%a in ("%AFTER_SLASH%") do (
-  set BEFORE_PERIOD=%%a
+for /f "tokens=1 delims=." %%a in ("%temp%") do (
+  set repo_name=%%a
 )
 
 cd /Users/%USERNAME%
 
 call git clone %url%
-cd %BEFORE_PERIOD%
+cd %repo_name%
 
 call mvn -Dmaven.test.failure.ignore=true -Djacoco.destFile=./coverage/jacoco.exec -Djacoco.dataFile=./coverage/jacoco.exec clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report
 
