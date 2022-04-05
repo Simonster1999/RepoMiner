343cac66593271229c66823dc556496aaea549e6
Mattias Ekdahl <matekd@student.chalmers.se>
2022-04-05 13:45:30 +0200

    Add clover tool, Add choice to remove cloned repo after using tools

--- RepoMiner.bat
+++ RepoMiner.bat
@@ -3,8 +3,10 @@ set /p url="Enter your git repository link here: "
 
 set origin=%CD%
 
+rem Determine is link is https or ssh
 if "%url:~0,5%" == "https" (set user_repo=%url:~19%) else (set user_repo=%url:~15%)
 
+rem Extract repository name from link
 for /f "tokens=2 delims=/" %%a in ("%user_repo%") do (
   set temp=%%a
 )
@@ -18,10 +20,29 @@ cd /Users/%USERNAME%
 call git clone %url%
 cd %repo_name%
 
+rem Tool: Jacoco
 call mvn -Dmaven.test.failure.ignore=true -Djacoco.destFile=./coverage/jacoco.exec -Djacoco.dataFile=./coverage/jacoco.exec clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report
 
 cd %origin%\XmlParser\out\production\XmlParser
 
 java Parser
 
+rem Tool: Clover
+cd /Users/%USERNAME%/%repo_name%
+
+call mvn -q clean compile
+
+call mvn -q -Dmaven.test.failure.ignore=true org.openclover:clover-maven-plugin:instrument 
+call mvn -q org.openclover:clover-maven-plugin:clover
+
+rem No parser yet
+
+rem Ending
+cd /Users/%USERNAME%/
+echo
+echo Do you wish to remove %repo_name%?
+rd /s "/Users/%USERNAME%/%repo_name%"
 cd %origin%
+
+
+
