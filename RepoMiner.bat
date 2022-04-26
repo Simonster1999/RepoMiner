4e6884d64eed86a8812e5bf8d4ae8b495a1254a5
Mattias Ekdahl <matekd@student.chalmers.se>
2022-04-26 15:48:10 +0200

    Allow repo names with '.', Add comments

--- RepoMiner.bat
+++ RepoMiner.bat
@@ -1,9 +1,10 @@
 @echo off
 set /p url="Enter your git repository link here: "
 
+rem Save starting directory
 set origin=%CD%
 
-rem Determine if link is https or ssh
+rem Determine if link is https or ssh, extract "user/repository.git"
 if "%url:~0,5%" == "https" (set user_repo=%url:~19%) else (set user_repo=%url:~15%)
 
 rem Extract repository name from link
@@ -11,16 +12,17 @@ for /f "tokens=2 delims=/" %%a in ("%user_repo%") do (
   set temp=%%a
 )
 
-for /f "tokens=1 delims=." %%a in ("%temp%") do (
-  set repo_name=%%a
-)
+rem Remove last 4 characters (.git)
+set repo_name=%temp:~0,-4%
 
+rem Navigate to user. All repositires will be cloned here
 cd /Users/%USERNAME%
 
 rem Clone and navigate to repository
 call git clone %url%
 cd %repo_name%
 
+rem Arguments to let projects build successfully
 set skip=-Drat.skip -Dcheckstyle.skip -Dmaven.test.failure.ignore=true
 
 rem --------------- Tool: Jacoco ---------------
@@ -34,6 +36,10 @@ cd target
 
 rem Args: Xmlpath, Tool
 java -jar Parser-1.0-SNAPSHOT-jar-with-dependencies.jar /%repo_name%/target/site/jacoco/jacoco.xml Jacoco
+
+rem --------------- After Jacoco ---------------
+rem Exclude Jacoco for other tools
+set skip=-Drat.skip -Dcheckstyle.skip -Dmaven.test.failure.ignore=true -Djacoco.skip=true
 
 rem --------------- Tool: Clover ---------------
 cd /Users/%USERNAME%/%repo_name%
